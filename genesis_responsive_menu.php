0dc825c6e8e5f81205803309311e3c41581ebbc3
kaolss <karin@kobotolo.se>
2014-07-04 19:05:42 +0200

    Changed to custom.css

--- genesis_responsive_menu.php
+++ genesis_responsive_menu.php
@@ -15,7 +15,8 @@ add_action( 'wp_enqueue_scripts', 'ok_responsive_css' );
 function ok_responsive_css() {
     wp_enqueue_script('nav_for_mobile', plugins_url('/js/drop-down-nav.js',  __FILE__), array('jquery'), '0.5' );	
 	wp_enqueue_style( 'genesis_responsive_menu', plugins_url('/css/genesis_responsive_menu.css', __FILE__), array(), '0.0.1' );
-	if ( file_exists( get_stylesheet_directory()."/genesis_responsive_menu.css" ) ) {
-		wp_enqueue_style( 'genesis_responsive_menu_custom', get_stylesheet_directory_uri() . '/genesis_responsive_menu.css', array(), '0.0.1' );
+	$cdir= plugin_dir_path(__FILE__);
+        if ( file_exists( $cdir."/css/custom.css")) { 
+        	wp_enqueue_style( 'genesis_responsive_menu_custom', plugins_url('/css/custom.css', __FILE__), array(), '0.0.1' );
 	}		
 }
